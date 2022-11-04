e003ff6ccc62c69e076be7e238e47eef700f65c4
Yvette Lerma <lerma.yvette@hotmail.com>
2022-11-04 09:12:03 -0600

    yvette you're doing great

---
+++ FactoryPattern/BabyUnicorn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FactoryPattern
+{
+    public class BabyUnicorn : IVehicle
+    {
+        public BabyUnicorn()
+        {
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("The baby unicorn has 3 legs. YAAAY!");
+        }
+    }
+}
+
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            switch (wheelCount)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 3:
+                    return new BabyUnicorn();
+                case 4:
+                    return new Car();
+                default:
+                    return new Car();
+
+            }
+        }
+    }
+}
+
